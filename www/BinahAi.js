83a152feda56eb844e251c0800f39cabffa7e160
marhano <carullomarjan@gmail.com>
2023-07-06 14:43:38 +0800

    .added binah sdk

--- www/BinahAi.js
+++ www/BinahAi.js
@@ -1,5 +1,11 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'BinahAi', 'coolMethod', [arg0]);
+var PLUGIN_NAME = "BinahAi";
+
+var BinahAi = function() {} 
+
+BinahAi.coolMethod = function (arg0, success, error) {
+    exec(success, error, PLUGIN_NAME, 'coolMethod', [arg0]);
 };
+
+module.exports = BinahAi;
