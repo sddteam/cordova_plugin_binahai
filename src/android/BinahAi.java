52b29124409d8671ca3bcbcd97c3cd387c8b5eb2
marhano <carullomarjan@gmail.com>
2023-07-12 10:21:58 +0800

    .change to fragment

--- src/android/BinahAi.java
+++ src/android/BinahAi.java
@@ -2,6 +2,15 @@ package inc.bastion.binahai;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -9,17 +18,44 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import ai.binah.sdk.api.HealthMonitorException;
+import ai.binah.sdk.api.SessionEnabledVitalSigns;
+import ai.binah.sdk.api.alerts.ErrorData;
+import ai.binah.sdk.api.alerts.WarningData;
+import ai.binah.sdk.api.images.ImageListener;
+import ai.binah.sdk.api.license.LicenseDetails;
+import ai.binah.sdk.api.license.LicenseInfo;
+import ai.binah.sdk.api.license.LicenseOfflineMeasurements;
+import ai.binah.sdk.api.session.Session;
+import ai.binah.sdk.api.session.SessionInfoListener;
+import ai.binah.sdk.api.session.SessionState;
+import ai.binah.sdk.api.session.demographics.Sex;
+import ai.binah.sdk.api.session.demographics.SubjectDemographic;
+import ai.binah.sdk.api.vital_signs.VitalSign;
+import ai.binah.sdk.api.vital_signs.VitalSignTypes;
+import ai.binah.sdk.api.vital_signs.VitalSignsListener;
+import ai.binah.sdk.api.vital_signs.VitalSignsResults;
+import ai.binah.sdk.api.vital_signs.vitals.VitalSignBloodPressure;
+import ai.binah.sdk.api.vital_signs.vitals.VitalSignPulseRate;
+import ai.binah.sdk.session.FaceSessionBuilder;
+
 /**
  * This class echoes a string called from JavaScript.
  */
-public class BinahAi extends CordovaPlugin {
+public class BinahAi extends CordovaPlugin implements TestActivity.ImagePreviewListener {
   private static final String TAG = "BinahAi";
+  private Session mSession;
+  private static final long MEASUREMENT_DURATION = 60;
 
   private static final String START_CAMERA = "startCamera";
   private static final String START_SCAN = "startScan";
 
   private CallbackContext startCameraCallbackContext;
-  private static final int START_CAMERA_REQUEST_CODE = 69;
+  private int containerViewId = 20;
+  private boolean toBack = true;
+  private ViewParent webViewParent;
+
+  private TestActivity fragment;
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -33,10 +69,65 @@ public class BinahAi extends CordovaPlugin {
 
   private boolean startCamera(CallbackContext callbackContext){
     startCameraCallbackContext = callbackContext;
+    final float opacity = Float.parseFloat("1");
+    fragment = new TestActivity();
+    fragment.setEventListener(this);
 
-    cordova.getActivity().runOnUiThread(() -> {
-      Intent intent = new Intent(cordova.getActivity(), CameraActivity.class);
-      cordova.startActivityForResult(this, intent, START_CAMERA_REQUEST_CODE);
+    cordova.getActivity().runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        //create or update the layout params for the container view
+        FrameLayout containerView = (FrameLayout)cordova.getActivity().findViewById(containerViewId);
+        if(containerView == null){
+          containerView = new FrameLayout(cordova.getActivity().getApplicationContext());
+          containerView.setId(containerViewId);
+
+          FrameLayout.LayoutParams containerLayoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+          cordova.getActivity().addContentView(containerView, containerLayoutParams);
+        }
+
+        //display camera below the webview
+        if(toBack){
+          View view = webView.getView();
+          ViewParent rootParent = containerView.getParent();
+          ViewParent curParent = view.getParent();
+
+          view.setBackgroundColor(0x00000000);
+
+          // If parents do not match look for.
+          if(curParent.getParent() != rootParent) {
+            while(curParent != null && curParent.getParent() != rootParent) {
+              curParent = curParent.getParent();
+            }
+
+            if(curParent != null) {
+              ((ViewGroup)curParent).setBackgroundColor(0x00000000);
+              ((ViewGroup)curParent).bringToFront();
+            } else {
+              // Do default...
+              curParent = view.getParent();
+              webViewParent = curParent;
+              ((ViewGroup)view).bringToFront();
+            }
+          }else{
+            // Default
+            webViewParent = curParent;
+            ((ViewGroup)curParent).bringToFront();
+          }
+
+        }else{
+          //set view back to front
+          containerView.setAlpha(opacity);
+          containerView.bringToFront();
+        }
+
+
+        FragmentManager fragmentManager = cordova.getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(containerView.getId(), fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+      }
     });
 
     return true;
@@ -44,6 +135,56 @@ public class BinahAi extends CordovaPlugin {
 
   private boolean startScan(CallbackContext callbackContext) {
     callbackContext.success(START_SCAN);
+    handleStartStopButtonClicked();
+
     return true;
+  }
+
+  @Override
+  public void onSessionCreated(Session session) {
+    this.mSession = session;
+  }
+
+  @Override
+  public void onFinalResult(VitalSignsResults vitalSignsResults) {
+    VitalSignBloodPressure bloodPressure =
+      (VitalSignBloodPressure) vitalSignsResults.getResult(VitalSignTypes.BLOOD_PRESSURE);
+
+    VitalSignPulseRate pulseRate =
+      (VitalSignPulseRate) vitalSignsResults.getResult(VitalSignTypes.PULSE_RATE);
+    String pulseRateValue = pulseRate != null ? pulseRate.getValue().toString() : "N/A";
+
+    String bloodPressureValue;
+    if (bloodPressure != null) {
+      bloodPressureValue = bloodPressure.getValue().getSystolic()
+        +"/"+ bloodPressure.getValue().getDiastolic();
+    } else {
+      bloodPressureValue = "N/A";
+    }
+  }
+
+  private void handleStartStopButtonClicked() {
+    if (mSession == null) {
+      return;
+    }
+
+    try {
+      if (mSession.getState() == SessionState.READY) {
+        mSession.start(MEASUREMENT_DURATION);
+      } else {
+        mSession.stop();
+      }
+    } catch (HealthMonitorException e) {
+      showAlert(null, "Error: " + e.getErrorCode());
+    }
+  }
+
+  private void showAlert(String title, String message) {
+    new AlertDialog.Builder(cordova.getActivity())
+      .setTitle(title)
+      .setMessage(message)
+      .setPositiveButton("OK", null)
+      .setCancelable(false)
+      .show();
   }
 }
--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -1,7 +1,5 @@
 package inc.bastion.binahai;
 
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
@@ -10,24 +8,22 @@ import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import org.apache.cordova.CordovaActivity;
-import org.apache.cordova.CordovaPlugin;
+import androidx.fragment.app.Fragment;
 
 import ai.binah.sdk.api.HealthMonitorException;
 import ai.binah.sdk.api.SessionEnabledVitalSigns;
-import ai.binah.sdk.api.alerts.AlertCodes;
 import ai.binah.sdk.api.alerts.ErrorData;
 import ai.binah.sdk.api.alerts.WarningData;
 import ai.binah.sdk.api.images.ImageData;
@@ -35,30 +31,27 @@ import ai.binah.sdk.api.images.ImageListener;
 import ai.binah.sdk.api.images.ImageValidity;
 import ai.binah.sdk.api.license.LicenseDetails;
 import ai.binah.sdk.api.license.LicenseInfo;
-import ai.binah.sdk.api.license.LicenseOfflineMeasurements;
 import ai.binah.sdk.api.session.Session;
 import ai.binah.sdk.api.session.SessionInfoListener;
 import ai.binah.sdk.api.session.SessionState;
 import ai.binah.sdk.api.session.demographics.Sex;
 import ai.binah.sdk.api.session.demographics.SubjectDemographic;
 import ai.binah.sdk.api.vital_signs.VitalSign;
-import ai.binah.sdk.api.vital_signs.VitalSignTypes;
 import ai.binah.sdk.api.vital_signs.VitalSignsListener;
 import ai.binah.sdk.api.vital_signs.VitalSignsResults;
-import ai.binah.sdk.api.vital_signs.vitals.VitalSignBloodPressure;
-import ai.binah.sdk.api.vital_signs.vitals.VitalSignOxygenSaturation;
-import ai.binah.sdk.api.vital_signs.vitals.VitalSignPulseRate;
-import ai.binah.sdk.api.vital_signs.vitals.VitalSignRespirationRate;
 import ai.binah.sdk.session.FaceSessionBuilder;
 import io.ionic.starter.R;
 
-public class CameraActivity extends CordovaActivity implements ImageListener, VitalSignsListener, SessionInfoListener {
-  private static final String TAG = "CameraActivity";
+public class TestActivity extends Fragment implements ImageListener, SessionInfoListener, VitalSignsListener{
+  public interface ImagePreviewListener{
+    void onSessionCreated(Session session);
+    void onFinalResult(VitalSignsResults vitalSignsResults);
+  }
+  private ImagePreviewListener eventListener;
+  private static final String TAG = "TestActivity";
 
-  private static final String LICENSE_KEY = "668765-6009B5-426FAD-D62FC0-D89858-19B9FF";
-  private static final long MEASUREMENT_DURATION = 60;
-  private static final String LOG_TAG = "BinahSample";
   private static final int PERMISSION_REQUEST_CODE = 12345;
+  private static final String LICENSE_KEY = "668765-6009B5-426FAD-D62FC0-D89858-19B9FF";
 
   private Session mSession;
   private Bitmap mFaceDetection;
@@ -67,63 +60,32 @@ public class CameraActivity extends CordovaActivity implements ImageListener, Vi
   private TextView _respirationRate;
   private TextView _oxygenSaturation;
   private Button _startStop;
+  private View _view;
 
+  private String appResourcePackage;
+
+  public void setEventListener(ImagePreviewListener listener){
+    eventListener = listener;
+  }
+
+  @Nullable
   @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_camera);
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    appResourcePackage = getActivity().getPackageName();
+    createSession();
+
+    _view = inflater.inflate(getResources().getIdentifier("activity_test", "layout", appResourcePackage), container, false);
     initUi();
-    _startStop.setOnClickListener(
-      (View.OnClickListener) v -> handleStartStopButtonClicked());
     mFaceDetection = createFaceDetectionBitmap();
-  }
 
-  private void initUi(){
-    _cameraView = findViewById(R.id.cameraView);
-    _pulseRate = findViewById(R.id.pulseRate);
-    _respirationRate = findViewById(R.id.respirationRate);
-    _oxygenSaturation = findViewById(R.id.oxygenSaturation);
-    _startStop = findViewById(R.id.startStopButton);
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-    int permissionStatus = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
-    if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
-      createSession();
-    } else {
-      requestPermissions((new String[]{android.Manifest.permission.CAMERA}), PERMISSION_REQUEST_CODE);
-    }
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    if (mSession != null) {
-      mSession.terminate();
-      mSession = null;
-    }
-  }
-
-  @Override
-  public void onRequestPermissionsResult(
-    int requestCode,
-    @NonNull String[] permissions,
-    @NonNull int[] grantResults
-  ) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    if (requestCode == PERMISSION_REQUEST_CODE
-      && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-      createSession();
-    }
+    return _view;
   }
 
   @Override
   public void onImage(ImageData imageData) {
-    runOnUiThread(() -> {
+    getActivity().runOnUiThread(() -> {
       if (imageData.getImageValidity() != ImageValidity.VALID) {
-        Log.i(LOG_TAG, "Image Validity Error: "+ imageData.getImageValidity());
+        Log.i(TAG, "Image Validity Error: "+ imageData.getImageValidity());
       }
       Canvas canvas = _cameraView.lockCanvas();
       if (canvas == null) {
@@ -155,171 +117,20 @@ public class CameraActivity extends CordovaActivity implements ImageListener, Vi
         canvas.drawBitmap(mFaceDetection, null, targetRect, null);
       }
 
-
       _cameraView.unlockCanvasAndPost(canvas);
     });
   }
 
-  @SuppressLint("SetTextI18n")
-  @Override
-  public void onVitalSign(VitalSign vitalSign) {
-    runOnUiThread(() -> {
-      if (vitalSign.getType() == VitalSignTypes.PULSE_RATE) {
-        VitalSignPulseRate pulseRate = (VitalSignPulseRate) vitalSign;
-        _pulseRate.setText("PR: "+ pulseRate.getValue());
-      }
-
-      if (vitalSign.getType() == VitalSignTypes.RESPIRATION_RATE) {
-        VitalSignRespirationRate respirationRate = (VitalSignRespirationRate) vitalSign;
-        _respirationRate.setText("RR: "+ respirationRate.getValue());
-        Log.i("VITAL SIGN", "Respiration Rate: " + respirationRate.getValue());
-      }
-
-      if (vitalSign.getType() == VitalSignTypes.OXYGEN_SATURATION) {
-        VitalSignOxygenSaturation oxygenSaturation = (VitalSignOxygenSaturation) vitalSign;
-        _oxygenSaturation.setText("OS: "+ oxygenSaturation.getValue());
-      }
-    });
-  }
-
-  @Override
-  public void onFinalResults(VitalSignsResults finalResults) {
-    runOnUiThread(() -> {
-      VitalSignBloodPressure bloodPressure =
-        (VitalSignBloodPressure) finalResults.getResult(VitalSignTypes.BLOOD_PRESSURE);
-
-      VitalSignPulseRate pulseRate =
-        (VitalSignPulseRate) finalResults.getResult(VitalSignTypes.PULSE_RATE);
-      String pulseRateValue = pulseRate != null ? pulseRate.getValue().toString() : "N/A";
-
-      String bloodPressureValue;
-      if (bloodPressure != null) {
-        bloodPressureValue = bloodPressure.getValue().getSystolic()
-          +"/"+ bloodPressure.getValue().getDiastolic();
-      } else {
-        bloodPressureValue = "N/A";
-      }
-
-      showAlert("Final Results", "Pulse Rate: "+ pulseRateValue +"\n"+ "Blood Pressure: "+ bloodPressureValue);
-    });
-  }
-
-  @Override
-  public void onSessionStateChange(SessionState sessionState) {
-    runOnUiThread(() -> {
-      switch (sessionState) {
-        case READY: {
-          _startStop.setEnabled(true);
-          _startStop.setText(R.string.start);
-          getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-          break;
-        }
-        case PROCESSING: {
-          _startStop.setEnabled(true);
-          _startStop.setText(R.string.stop);
-          getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-          break;
-        }
-        default: {
-          _startStop.setEnabled(false);
-          getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-          break;
-        }
-      }
-    });
-  }
-
-  @Override
-  public void onWarning(WarningData warningData) {
-    runOnUiThread(() -> {
-      if (warningData.getCode() == AlertCodes.MEASUREMENT_CODE_MISDETECTION_DURATION_EXCEEDS_LIMIT_WARNING) {
-        _pulseRate.setText("");
-      }
-
-      Toast.makeText(this, "Warning: " + warningData.getCode(), Toast.LENGTH_SHORT).show();
-    });
-  }
-
-  @Override
-  public void onError(ErrorData errorData) {
-    runOnUiThread(() -> {
-      showAlert(null, "Error: "+ errorData.getCode());
-    });
-  }
-
-  @Override
-  public void onLicenseInfo(LicenseInfo licenseInfo) {
-    runOnUiThread(() -> {
-      String activationId = licenseInfo.getLicenseActivationInfo().getActivationID();
-      if (!activationId.isEmpty()) {
-        Log.i(LOG_TAG, "License Activation ID: "+ activationId);
-      }
-
-      LicenseOfflineMeasurements offlineMeasurements = licenseInfo.getLicenseOfflineMeasurements();
-      if (offlineMeasurements != null) {
-        Log.i(LOG_TAG,
-          "License Offline Measurements: " +
-            offlineMeasurements.getTotalMeasurements() +"/" +
-            offlineMeasurements.getRemainingMeasurements()
-        );
-      }
-    });
-  }
-
-  @Override
-  public void onEnabledVitalSigns(SessionEnabledVitalSigns sessionEnabledVitalSigns) {
-    runOnUiThread(() -> {
-      Log.i(LOG_TAG,"Pulse Rate Enabled: "+ sessionEnabledVitalSigns.isEnabled(VitalSignTypes.PULSE_RATE));
-      Log.i(LOG_TAG,"Respiration Rate Enabled: "+ sessionEnabledVitalSigns.isEnabled(VitalSignTypes.RESPIRATION_RATE));
-      Log.i(LOG_TAG,"Oxygen Saturation Enabled: "+ sessionEnabledVitalSigns.isEnabled(VitalSignTypes.OXYGEN_SATURATION));
-    });
-  }
-
-  private void createSession() {
-    LicenseDetails licenseDetails = new LicenseDetails(LICENSE_KEY);
-    try {
-      SubjectDemographic subjectDemographic = new SubjectDemographic(Sex.MALE, 23.0, 52.0);
-      mSession = new FaceSessionBuilder(getApplicationContext())
-        .withSubjectDemographic(subjectDemographic)
-        .withImageListener(this)
-        .withVitalSignsListener(this)
-        .withSessionInfoListener(this)
-        .build(licenseDetails);
-    } catch (HealthMonitorException e) {
-      showAlert(null, "Error: " + e.getErrorCode());
-    }
-  }
-
-  private void handleStartStopButtonClicked() {
-    if (mSession == null) {
-      return;
-    }
-
-    try {
-      if (mSession.getState() == SessionState.READY) {
-        mSession.start(MEASUREMENT_DURATION);
-        _pulseRate.setText("");
-        _respirationRate.setText("");
-        _oxygenSaturation.setText("");
-      } else {
-        mSession.stop();
-      }
-    } catch (HealthMonitorException e) {
-      showAlert(null, "Error: " + e.getErrorCode());
-    }
-  }
-
-  private void showAlert(String title, String message) {
-    new AlertDialog.Builder(this)
-      .setTitle(title)
-      .setMessage(message)
-      .setPositiveButton("OK", null)
-      .setCancelable(false)
-      .show();
+  private void initUi(){
+    _cameraView = (TextureView) _view.findViewById(R.id.cameraView);
+    _pulseRate = (TextView) _view.findViewById(R.id.pulseRate);
+    _respirationRate = _view.findViewById(R.id.respirationRate);
+    _oxygenSaturation = _view.findViewById(R.id.oxygenSaturation);
+    _startStop = _view.findViewById(R.id.startStopButton);
   }
 
   private Bitmap createFaceDetectionBitmap() {
-    Drawable drawable = ContextCompat.getDrawable(this, R.drawable.face_detection);
+    Drawable drawable = ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.face_detection);
     if (drawable == null) {
       return null;
     }
@@ -336,4 +147,67 @@ public class CameraActivity extends CordovaActivity implements ImageListener, Vi
 
     return bitmap;
   }
+
+  private void createSession() {
+    LicenseDetails licenseDetails = new LicenseDetails(LICENSE_KEY);
+    try {
+      SubjectDemographic subjectDemographic = new SubjectDemographic(Sex.MALE, 23.0, 52.0);
+      mSession = new FaceSessionBuilder(getActivity().getApplicationContext())
+        .withSubjectDemographic(subjectDemographic)
+        .withImageListener(this)
+        .withVitalSignsListener(this)
+        .withSessionInfoListener(this)
+        .build(licenseDetails);
+
+      eventListener.onSessionCreated(mSession);
+
+    } catch (HealthMonitorException e) {
+      showAlert(null, "Error: " + e.getErrorCode());
+    }
+  }
+
+  private void showAlert(String title, String message) {
+    new AlertDialog.Builder(getActivity())
+      .setTitle(title)
+      .setMessage(message)
+      .setPositiveButton("OK", null)
+      .setCancelable(false)
+      .show();
+  }
+
+  @Override
+  public void onSessionStateChange(SessionState sessionState) {
+
+  }
+
+  @Override
+  public void onWarning(WarningData warningData) {
+
+  }
+
+  @Override
+  public void onError(ErrorData errorData) {
+
+  }
+
+  @Override
+  public void onLicenseInfo(LicenseInfo licenseInfo) {
+
+  }
+
+  @Override
+  public void onEnabledVitalSigns(SessionEnabledVitalSigns sessionEnabledVitalSigns) {
+
+  }
+
+  @Override
+  public void onVitalSign(VitalSign vitalSign) {
+
+  }
+
+  @Override
+  public void onFinalResults(VitalSignsResults vitalSignsResults) {
+    eventListener.onFinalResult(vitalSignsResults);
+  }
+
 }
