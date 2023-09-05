a9233875c5f3bee1e2a467f0e5e9a9ad2ff18317
marhano <carullomarjan@gmail.com>
2023-09-05 14:59:11 +0800

    .test decription

--- tests/tests.js
+++ tests/tests.js
@@ -1,11 +1,50 @@
 exports.defineAutoTests = function(){
-    describe('BinahAi', function(){
-        it('should have a method startCamera', function(){
-            expect(BinahAi.startCamera).toBeDefined();
+    describe('BinahAi Plugin', function() {
+        var onSuccess = function() {
+            // Success callback for testing
+        };
+    
+        var onError = function() {
+            // Error callback for testing
+        };
+    
+        beforeEach(function() {
+            // Set up any necessary environment or mock Cordova's exec function
+            spyOn(window.cordova, 'exec');
         });
     
-        it('should return a string when calling startCamera', function(){
-            expect(BinahAi.startCamera()).toEqual(jasmine.any(String));
+        it('should define BinahAi object', function() {
+            expect(BinahAi).toBeDefined();
+        });
+    
+        it('should define startCamera method', function() {
+            expect(BinahAi.startCamera).toBeDefined();
+            expect(typeof BinahAi.startCamera).toBe('function');
+        });
+
+        it('should define stopCamera method', function() {
+            expect(BinahAi.stopCamera).toBeDefined();
+            expect(typeof BinahAi.stopCamera).toBe('function');
+        });
+
+        it('should define startScan method', function() {
+            expect(BinahAi.startScan).toBeDefined();
+            expect(typeof BinahAi.startScan).toBe('function');
+        });
+
+        it('should define stopScan method', function() {
+            expect(BinahAi.stopScan).toBeDefined();
+            expect(typeof BinahAi.stopScan).toBe('function');
+        });
+
+        it('should define imageValidation method', function() {
+            expect(BinahAi.imageValidation).toBeDefined();
+            expect(typeof BinahAi.imageValidation).toBe('function');
+        });
+
+        it('should define getSessionState method', function() {
+            expect(BinahAi.getSessionState).toBeDefined();
+            expect(typeof BinahAi.getSessionState).toBe('function');
         });
     });
 };
